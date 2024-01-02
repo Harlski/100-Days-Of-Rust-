cec3bf1b2da0ee31c4e0342ec4a5d8311020dac1
Harlski <harley.jones1992@gmail.com>
2024-01-02 11:38:01 +1100

    Day 01 Completed

---
+++ Week-01/Day-01_Convert-Ages-To-Days/my_convert_ages_to_days/target/debug/deps/my_convert_ages_to_days.d
@@ -0,0 +1,5 @@
+C:\Users\Harlski\100-Days-Of-Rust\Week-01\Day-01_Convert-Ages-To-Days\my_convert_ages_to_days\target\debug\deps\my_convert_ages_to_days.exe: src\main.rs
+
+C:\Users\Harlski\100-Days-Of-Rust\Week-01\Day-01_Convert-Ages-To-Days\my_convert_ages_to_days\target\debug\deps\my_convert_ages_to_days.d: src\main.rs
+
+src\main.rs:

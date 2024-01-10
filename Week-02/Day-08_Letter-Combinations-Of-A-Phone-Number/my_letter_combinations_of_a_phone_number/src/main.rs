c4f05c25c12cd3a473dafda4ecc8844774e47d95
Harley <harley.jones1992@gmail.com>
2024-01-10 23:03:09 +1100

    Almost solved :o, final hurdle

--- Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/my_letter_combinations_of_a_phone_number/src/main.rs
+++ Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/my_letter_combinations_of_a_phone_number/src/main.rs
@@ -3,8 +3,19 @@
 // Feel like I should know how to do this by now, but nothing is sticking.
 // Probably a really simple way to do this, but I'm determined not to influence my answer with an existing one
 // If it takes me an entire week, I'll keep going at this problem.
-use std::io;
 
+// So I am not far off solving this
+// As of this commit, I am able to output the correct answer for 4 digits
+// However any numbers lower currently produces error
+// I'm currently refactoring to ensure that the correct while loop is run depending on the length of the digits
+// Don't know if this optimal, however I like to think it looks tidy/clean.
+// The issue/problem I'm facing is getting type declared initially for  j_char, j_char_len j_chars (&str, usize, Vec<char>) for (j,k,l)
+// I'm not sure what the best practie is to handle this, so I'm just test as I go.
+// I'm think the best might be to set placeholder values and overwrite if the digits.len() condition is met
+// Because if the values are set and digits.len() isn't met then they're not going to be used anyway.
+// 
+use std::io;
+use std::any::type_name;
 fn main() {
     let mut digits = String::new();
 
@@ -24,6 +35,9 @@ fn main() {
     println!("Answer: {:?}", return_string_combo(&digits.trim()));
 }
 
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
 fn return_string_combo(digits: &str) -> Vec<String> {
     let letters = vec!["abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"];
@@ -31,28 +45,95 @@ fn return_string_combo(digits: &str) -> Vec<String> {
     let mut v: Vec<String> = Vec::new();
 
     let mut i = 0; // First Digit
-    let mut j = 1; // Second Digit
-    let mut k = 2; // Third Digit
-    let mut l = 3; // Fourth Digit
-    // let mut current = "";
-    let char_g: Vec<_> = letters[2].chars().collect();
-    println!("What the digits: {:?}", digits);
-    if 
-    if digits.len() >= 4 {
-        v.push(digits[i].to_string() + &digits[j].to_string() + &digits[k].to_string() + &digits[l].to_string() ); // This pushes current
-    }
-    if digits.len() >= 3 {
-        v.push(digits[i].to_string() + &digits[j].to_string() + &digits[k].to_string() ); // This pushes current
-    }
+    let mut j = 0; // Second Digit
+    let mut k = 0; // Third Digit
+    let mut l = 0; // Fourth Digit
+    println!("Type of i: {}", type_of(&i));
+
+    let i_char = letters[digits[0] as usize - 50]; // We remove 48 for the usize conversion and then 2 to match the number to the index
+    let i_chars = i_char.chars().collect::<Vec<_>>();
+    let i_char_len = i_char.len();
+    // Type i_char, i_chars, i_char_len: &str "alloc::vec::Vec<char>" usize
+    // println!("Type i_char, i_chars, i_char_len: {} {:?} {}", type_of(&i_char), type_of(&i_chars), type_of(&i_char_len));
+    let (j_char, k_char, l_char) = ("","","");
+    let (j_chars, k_chars, l_chars): (Vec<char>, Vec<char>, Vec<char>) = vec![];
+    let (j_char_len, k_char_len, l_char_len) = (0, 0, 0);
+
     if digits.len() >= 2 {
-        v.push(digits[i].to_string() + &digits[j].to_string() ); // This pushes current
+        let j_char = letters[digits[1] as usize - 50];
+        let j_chars = j_char.chars().collect::<Vec<_>>();
+        let j_char_len = j_char.len();
+    }  
+    if digits.len() >= 3 {
+        let k_char = letters[digits[2] as usize - 50];
+        let k_chars = k_char.chars().collect::<Vec<_>>();
+        let k_char_len = k_char.len();
     }
+    if digits.len() >= 4 {
+        let l_char = letters[digits[3] as usize - 50];
+        let l_chars = l_char.chars().collect::<Vec<_>>();
+        let l_char_len = l_char.len();
+    }
+
+    // let char_at: Vec<_> = letters[i as usize].chars().collect();
+
+    // println!("i: {}, j:{}, k:{}, l{}", i, j, k ,l);
+    // println!("i_char: {}, j_char: {}, k_char: {}, l_char: {}", i_char, j_char, k_char, l_char);
+    // println!("Length: i_char: {:?}", i_char.chars().collect::<Vec<_>>());
+    // println!("Length i_char: {}, Length j_char: {}, Length k_char: {}, Length l_char: {}", i_char_len, j_char_len, k_char_len, l_char_len);
+        println!("i: {}, j:{}, k:{}, l{}", i, j, k ,l);
+        // println!("string: {}", i_chars[i].to_string() + &j_chars[j].to_string() + &k_chars[k].to_string() + &l_chars[l].to_string());
+
+        // println!("We in here");
+
+        // if i == i_char_len && j == j_char_len && k == k_char_len && l == l_char_len { println!("Should exit here"); return v; }
+        if digits.len() == 4 { 
+            while i <= i_char_len -1 && j <= j_char_len && k <= k_char_len && l <= l_char_len {
+                v.push(i_chars[i].to_string() + &j_chars[j].to_string() + &k_chars[k].to_string() + &l_chars[l].to_string()); // This pushes current
+                if l <= l_char_len { l += 1; if l == l_char_len { k += 1; l = 0; } } 
+                if k == k_char_len { j += 1; k = 0}   
+                if j == j_char_len { i += 1; j = 0}  
+            }
+
+            println!("Length 4" );
+        }
+        if digits.len() == 3 { println!("Length 3" );}
+        if digits.len() == 2 { println!("Length 2" );}
+        if digits.len() == 1 { println!("Length 1" );}
+
+        // if l <= l_char_len { l += 1; if l == l_char_len { k += 1; l = 0; } } 
+        // if k == k_char_len { j += 1; k = 0}   
+        // if j == j_char_len { i += 1; j = 0}  
+   
+    println!("What the digits: {:?}", digits);
+    if digits.len() == 4 { println!("Length 4" );}
+
+    v
+}
+    // let mut current = "";
+
+    // while i <= digits.len() {
+    //     if digits.len() >= 4 {
+    //         v.push(digits[i].to_string() + &digits[j].to_string() + &digits[k].to_string() + &digits[l].to_string() ); // This pushes current
+    //     }
+    //     if digits.len() >= 3 {
+    //         v.push(digits[i].to_string() + &digits[j].to_string() + &digits[k].to_string() ); // This pushes current
+    //     }
+    //     if digits.len() >= 2 {
+    //         v.push(digits[i].to_string() + &digits[j].to_string() ); // This pushes current
+    //     } 
+    //     if digits.len() >=1 {
+    //         v.push(char_at[digits[0] as usize -48].to_string()); // This pushes current
+    //         i += 1;
+
+    //     }
+    // }
     // if i >= digits[0].len() && j >= digits[1].len() && k >= digits[2].len() && l >= digits[3].len() {
     //     return v;
     // }
 
 
-    // println!("What I expect to be g: {:?} ", char_g[0]);
+    // println!("What I expect to be g: {:?} ", char_at[0]);
     // while i <= digits.len() {
     //     v.push(letters )
     // }
@@ -98,8 +179,7 @@ fn return_string_combo(digits: &str) -> Vec<String> {
     //     // for i <= letters
 
     // }
-    v
-}
+
 
 // 2 3 4
 // abc def ghi
